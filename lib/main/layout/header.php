43e856841b934830a84c37012dbb98e33db5febf
maderami <sdsoerty6584@gmail.com>
2024-09-30 20:29:31 +0600

    edit of the header file

--- lib/main/layout/header.php
+++ lib/main/layout/header.php
@@ -17,6 +17,15 @@
             <h2 class="title-link">
                 IC "Repair Design Project"
             </h2>
+            <ul class="nav-button">
+                <li><a>Home</a></li>
+                <li><a>Projects</a></li>
+                <li><a>MEASUREMENT</a></li>
+                <li><a>Team</a></li>
+                <li><a>Reviews</a></li>
+                <li><a>Contacts</a></li>
+            </ul>
+            <button class="button-function"><span>Request a call</span></button>
         </div>
     </div>
 </header>
